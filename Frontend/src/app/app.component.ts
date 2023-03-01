8148dbec0b4904e84c07075d04b76b4e17f2bf1a
unknown <jameskagunga2022@gmail.com>
2023-03-01 09:28:59 -0800

    added the chatarea

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Frontend';
+  form!: FormGroup;
+
+  ngOnInit(): void {
+
+    this.form = new FormGroup(
+      {
+        message: new FormControl(null, [
+          Validators.required
+        ])
+      }
+    )
+
+  }
+
+  chats = [
+    {
+      user: "gpt",
+      message: "How can I help you today?",
+    },
+
+    {
+      user: "me",
+      message: "I wanted to use chatgpt today",
+    },
+  ]
+
+  onSubmit() {
+  this.chats.push({user:"me",message:this.form.value.message})   
+
+ 
+
+  }
+
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+   
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
